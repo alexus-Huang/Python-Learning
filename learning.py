042b6bee424070e8d3b09f870c54cae3fd4e4e0b
Alexus Huang <alexushu2325@gmail.com>
2024-01-21 11:09:15 -0800

    Updated add_three function

--- learning.py
+++ learning.py
@@ -1,6 +1,6 @@
 # Functions
-def add_three(input_var):
+def add_three(input_var,second_var):
     output_var=input_var+3
-    return output_var
+    return output_var,second_var
 
-print(add_three(3))
+print(add_three(3,5))
