f92acb8dd365666e8d1527083fd60bf7fbf82966
Alexus Huang <alexushu2325@gmail.com>
2024-01-23 17:13:47 -0800

    Used the round function

--- learning.py
+++ learning.py
@@ -1,30 +1,34 @@
-# Functions
-def add_three(input_var):
-    output_var=input_var+3
-    return output_var
+# # Functions
+# def add_three(input_var):
+#     output_var=input_var+3
+#     return output_var
 
-new_number = add_three(3)
-# print(new_number)
+# new_number = add_three(3)
+# # print(new_number)
 
-# My own function
-def sayHello():
-    return "Hello"
+# # My own function
+# def sayHello():
+#     return "Hello"
 
-# print(sayHello())
+# # print(sayHello())
 
 
-#-------------------------------
-# Using math.ceil()
-import math
-a_number=1.1
-rounded_value=math.ceil(a_number)
-# print("Rounded number is", rounded_value)
+# #-------------------------------
+# # Using math.ceil()
+# import math
+# a_number=1.1
+# rounded_value=math.ceil(a_number)
+# # print("Rounded number is", rounded_value)
 
-# Data types
-number = 10
-print(number)
-print(type(number))
+# # Data types
+# number = 10
+# print(number)
+# print(type(number))
 
-floatingNumber =10.1
-print(floatingNumber)
-print(type(floatingNumber))
+# floatingNumber =10.1
+# print(floatingNumber)
+# print(type(floatingNumber))
+
+roundNumber =5.147632
+rounded = round(roundNumber,3)
+print(rounded)
