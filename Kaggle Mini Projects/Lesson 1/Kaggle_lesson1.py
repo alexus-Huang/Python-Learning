1bf714b3060c1d169d3b1723b80cf8d1f2c94002
Alexus Huang <alexushu2325@gmail.com>
2024-02-17 20:53:38 -0800

    Finished the first mini project for Kaggle Mini Projecst in Kaggle_lesson1.py

---
+++ Kaggle Mini Projects/Lesson 1/Kaggle_lesson1.py
@@ -0,0 +1,26 @@
+whole_number = 5 #integer
+decimal_number = 5.22 #float
+a_word = "Word" #string
+
+print(whole_number) # Print out each of the assigned variables (3 in total)
+print(decimal_number) # Print out each of the assigned variables (3 in total)
+print(a_word) # Print out each of the assigned variables (3 in total)
+
+multiple_words = "Words" * 3 # Assign a variable to a string that is multiplied by 3
+print(multiple_words)
+print(type(whole_number)) # Print out the type of variable for any of the assigned variables above
+
+# Basic arithmetic skills
+print(5%2)
+print(2/4)
+print(2*8)
+print(2**5)
+print(5+9)
+
+# min and max function
+print(min(32154,111,4422,7354,1234,31254,5312,32140))
+print(max(32154,111,4422,7354,1234,31254,5312,32140))
+
+# abs function
+
+print(abs(-2))
