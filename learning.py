3d70fa0000d9b1d53eaed3737f7f4603d92fe715
Alexus Huang <alexushu2325@gmail.com>
2024-02-21 20:24:30 -0800

    Updated learning.py

--- learning.py
+++ learning.py
@@ -1,2 +1,5 @@
-test = input("What is your name?")
-print(test)
+name=input("What is your name:")
+age=int(input("How old are you:"))
+height=float(input("How tall are you in cm?"))
+
+print("Your name is:" + str(name) + "\n You are " + str(age) + "y/o \n Your height is " + str(height) +"cm")
