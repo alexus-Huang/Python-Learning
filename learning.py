91c782406d34c2afeeb1b1c54e1b697fd7d21442
Alexus Huang <alexushu2325@gmail.com>
2024-02-22 20:16:56 -0800

    Learned about the logical operator 'or'

--- learning.py
+++ learning.py
@@ -1,6 +1,7 @@
-feeling=input("How are you feeling today?")
-any_trouble=input("Did any trouble happen?")
-if feeling.lower() == "great" and any_trouble.lower() == "no":
-    print ("You are feeling {} today".format(feeling.lower()))
+how_many_tires=int(input("How many tires do you have on your vechile "))
+if how_many_tires==4 or how_many_tires==3:
+    print("You currently have {} tires on your vechile!".format(how_many_tires))
+elif how_many_tires > 4:
+    print("You have more than 4 tires!")
 else:
-    print("Oh no")
+    print ("You don't have a car")
