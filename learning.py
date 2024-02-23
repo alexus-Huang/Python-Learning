3aae1ecdf6f0cbb272555ab73d6124d86072d496
Alexus Huang <alexushu2325@gmail.com>
2024-02-22 20:20:29 -0800

    Learned about the logical operator 'not'

--- learning.py
+++ learning.py
@@ -1,7 +1,5 @@
-how_many_tires=int(input("How many tires do you have on your vechile "))
-if how_many_tires==4 or how_many_tires==3:
-    print("You currently have {} tires on your vechile!".format(how_many_tires))
-elif how_many_tires > 4:
-    print("You have more than 4 tires!")
+feeling=input("How are you feeling today? ")
+if not(feeling.lower() == "great"):
+    print("Oh no")
 else:
-    print ("You don't have a car")
+    print("You are feeling {}".format(feeling))
