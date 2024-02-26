993f7aa24cfd37db8e8fc0b55559e02f6a92dc96
Alexus Huang <alexushu2325@gmail.com>
2024-02-25 19:03:10 -0800

    Learned about continue

--- learning.py
+++ learning.py
@@ -1,5 +1,16 @@
-#break - used to stop the loop
-while True:
-    name=input("Enter your name:")
-    if name !="":
-        break
+# #break - used to stop the loop
+# while True:
+#     name=input("Enter your name:")
+#     if name !="":
+#         break
+
+
+
+#continue - skips to the next set of lines in the loop
+phone="123-456-7890"
+for i in phone:
+    if i == "-":
+        continue
+    print(i,end="")
+
+
