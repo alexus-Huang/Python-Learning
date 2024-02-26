91d5695c6f79de5ba5795b3140f99147ef37ec6a
Alexus Huang <alexushu2325@gmail.com>
2024-02-25 19:22:15 -0800

    Learned about lists

--- learning.py
+++ learning.py
@@ -1,23 +1,3 @@
-# #break - used to stop the loop
-# while True:
-#     name=input("Enter your name:")
-#     if name !="":
-#         break
-
-
-
-#continue - skips to the next set of lines in the loop
-# phone="123-456-7890"
-# for i in phone:
-#     if i == "-":
-#         continue
-#     print(i,end="")
-
-
-#pass - does nothing, acts as a placeholder
-    
-for i in range(1,21):
-    if i == 13:
-        pass
-    else:
-        print(i)
+food = ["pizza","drink","cookie"]
+food.clear()
+print(food)
