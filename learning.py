f9204ea46a48a9452af1ad726b5bda0cbf475ef1
Alexus Huang <alexushu2325@gmail.com>
2024-03-06 21:09:01 -0800

    Learned about nested functions

--- learning.py
+++ learning.py
@@ -1,6 +1,3 @@
-# functions
+# nested functions
 
-def greeting(first_name,last_name,middle_name):
-    print("Hello " + first_name + " " + middle_name +" " + last_name)
-
-greeting(middle_name="K", last_name="Doe", first_name="John")
+print(float(input("Enter in a number")))
