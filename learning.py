a39e01fd4cc72c07d6e9407d762c2ef8853e81c4
Alexus Huang <alexushu2325@gmail.com>
2024-03-06 21:04:48 -0800

    Learned about keyword arguments

--- learning.py
+++ learning.py
@@ -1,8 +1,6 @@
 # functions
 
-a_name = "John"
-def hello(name):
-    print("Hello " + name)
+def greeting(first_name,last_name,middle_name):
+    print("Hello " + first_name + " " + middle_name +" " + last_name)
 
-
-hello(a_name)
+greeting(middle_name="K", last_name="Doe", first_name="John")
