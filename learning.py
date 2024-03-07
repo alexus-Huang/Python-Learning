a953f28978902b7c78ec4f84197e61d055f48aa1
Alexus Huang <alexushu2325@gmail.com>
2024-03-06 21:33:28 -0800

    Added space after the first name

--- learning.py
+++ learning.py
@@ -1,4 +1,5 @@
 # str.format()
 
 
-print("Hello {first_name} {last_name} {middle_name}".format(first_name="john",last_name="doe",middle_name="L"))
+text= "Hello {first_name:50} {last_name}"
+print(text.format(first_name="John",last_name="Doe"))
