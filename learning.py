6c859bfc4aa79cf132c759bc1f443de254e1ff9b
Alexus Huang <alexushu2325@gmail.com>
2024-03-06 21:29:38 -0800

    Learned about kwargs in str.format()

--- learning.py
+++ learning.py
@@ -1,7 +1,4 @@
 # str.format()
 
-first_name = "John"
-last_name = "Doe"
-middle_name = "L"
 
-print("Hello {1} {2} {0}".format(first_name,last_name,middle_name))
+print("Hello {first_name} {last_name} {middle_name}".format(first_name="john",last_name="doe",middle_name="L"))
