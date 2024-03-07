559c48ae4d5a5b30656a68059bc87192c5f42b7f
Alexus Huang <alexushu2325@gmail.com>
2024-03-06 21:16:01 -0800

    Learned about args

--- learning.py
+++ learning.py
@@ -1,3 +1,9 @@
-# nested functions
+# *args
 
-print(float(input("Enter in a number")))
+def add(*all_numbers):
+    sum = 0
+    for i in all_numbers:
+        sum+=i
+    print(sum)
+
+add(1,2,3,4,5,6)
