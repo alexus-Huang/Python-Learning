61fb40e2035a9df5fa7e488aa90055d2332b4994
Alexus Huang <alexushu2325@gmail.com>
2024-03-13 20:52:18 -0700

    Practiced everything that was learned

--- learning.py
+++ learning.py
@@ -1,2 +1,21 @@
-FAVORITE_NUMBER = 14  # variable that sets what my favorite number is
-print(f"Favorite number: {FAVORITE_NUMBER}") # prints out the value of favorite_number
+"""
+Practice:
+- underscores in numbers
+- multiple assignment
+- constants
+- practice writing good comments to explain whats going on in the code
+- stripping whitespace
+- use the format method to print stuff out
+"""
+
+one_thousand = 1_000 # underscores in numbers to make it easier to read numbers
+x,y,z=1,2,3 # multiple assignment practice 
+THIS_MONTH = 3 # constant variable by using capital letters to indicate that the variable is a constant
+filename = "python_file.py" # filename variable containing a string to depict a file name
+URL = "https://www.google.com" # constant variable 
+
+print(one_thousand)
+print(x,y,z, sep=" ")
+print(THIS_MONTH)
+print(f"File name: {filename.removesuffix('.py')}")
+print(f"URL: {URL.removeprefix('https://')}")
