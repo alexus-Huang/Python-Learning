44baca6a8d2892533223d6694314d6c3a0166782
Alexus Huang <alexushu2325@gmail.com>
2024-03-15 22:48:53 -0700

    Completed 3.5. Changing Guest List in Python Crash Course Book

--- learning.py
+++ learning.py
@@ -1,2 +1,10 @@
 guest_list= ["John Doe", "Jane Doe","Bob","Jack","Dave"]
 print(f"This is the guest list: {guest_list[0]}, {guest_list[1]}, {guest_list[2]}, {guest_list[3]}, {guest_list[4]}")
+
+# 3.5 - changing the guest list
+unable = "Bob"
+guest_list.remove(unable) # Remove BOb from guest_list
+print(f"Looks like: {unable} can't make it.")
+replacement= "Zack"  # Set a variable and assign a value of who is replacing Bob
+guest_list.append(replacement) # Add in the person that will be replacing Bob
+print(f"This is the guest list: {guest_list[0]}, {guest_list[1]}, {guest_list[2]}, {guest_list[3]}, {guest_list[4]}")  # Print the new list
