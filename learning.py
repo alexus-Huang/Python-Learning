efe1a288366659a03cd344a9a418b0ae8e061f66
Alexus Huang <alexushu2325@gmail.com>
2024-03-15 22:43:35 -0700

    Completed 3.4. Guest list in Python Crash Course book

--- learning.py
+++ learning.py
@@ -1,6 +1,2 @@
-names=["John","Jack","bob"]
-in_trouble= "John"
-print(names)
-names.remove(in_trouble)
-print(names)
-print(f"{in_trouble} was in trouble")
+guest_list= ["John Doe", "Jane Doe","Bob","Jack","Dave"]
+print(f"This is the guest list: {guest_list[0]}, {guest_list[1]}, {guest_list[2]}, {guest_list[3]}, {guest_list[4]}")
