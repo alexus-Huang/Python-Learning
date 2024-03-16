f7bf45021fd27ebc4ccb76d51b06d5dfcf13b405
Alexus Huang <alexushu2325@gmail.com>
2024-03-15 22:51:56 -0700

    Completed 3.6. More Guests from Python Crash Course Book

--- learning.py
+++ learning.py
@@ -8,3 +8,11 @@ print(f"Looks like: {unable} can't make it.")
 replacement= "Zack"  # Set a variable and assign a value of who is replacing Bob
 guest_list.append(replacement) # Add in the person that will be replacing Bob
 print(f"This is the guest list: {guest_list[0]}, {guest_list[1]}, {guest_list[2]}, {guest_list[3]}, {guest_list[4]}")  # Print the new list
+
+
+# 3.6 - More Guests
+print("Looks like we found a bigger table\n")
+guest_list.insert(0,"Pheobe")
+guest_list.insert(2,"Scott")
+guest_list.append("Jesse")
+print(f"This is the guest list: {guest_list[0]}, {guest_list[1]}, {guest_list[2]}, {guest_list[3]}, {guest_list[4]}, {guest_list[5]}, {guest_list[6]}, {guest_list[7]}")
