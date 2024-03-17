162e8bb25a99b6b56685a1550123321e0418ad0b
Alexus Huang <alexushu2325@gmail.com>
2024-03-16 19:19:28 -0700

    Completed 3.8. Seeing the World from the Python Crash Course book

--- learning.py
+++ learning.py
@@ -1,5 +1,16 @@
-food =["banana","cranberry","kiwi","pineapple","lettuce","celery","apple"]
+# 3-8 Seeing the World
 
-print(f"Here is the original list: {food}")
-print(f"Here is the sorted list: {sorted(food)}")
-print(f"Here is the original list again: {food}")
+locations = ["California","Arizona","Texas","Chicago","Japan"] # 5 places to visit
+print(f"Here is the original list: {locations}") # Show the user the 5 locations
+print(f"Here is the sorted locations: {sorted(locations)}") # Print the temporary sorted list of locations
+print(f"Here is the original list again: {locations}") # Show that the 5 locations are still in its original order
+print(f"Here is the list in reverse-alphabetical order: {sorted(locations,reverse=True)}")
+print(f"Here is the original list again: {locations}") # Show that the 5 locations are still in its original order
+locations.reverse() # Reverse the order of the locations
+print(f"Here is the list in reverse:{locations}") # Show the reversed list
+locations.reverse() # Reverse the order of the locations again
+print(f"Here is the list in reverse:{locations}") # Show the original list
+locations.sort() # Permanently sort the list
+print(f"Here is locations in alphabetical order:{locations}") # Show that the list has been permanently sorted
+locations.sort(reverse=True)
+print(f"Here is the list of locations in reverse-alphabetical order: {locations}")
