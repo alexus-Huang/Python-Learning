e1b7075addef950340eebdd613c9671e04ee0c89
Alexus Huang <alexushu2325@gmail.com>
2024-03-17 18:03:29 -0700

    Learned about the range() function

--- learning.py
+++ learning.py
@@ -1,11 +1,2 @@
-# 4.1. Pizzas
-pizzas = ["pepporoni","cheese","pineapple"]
-for pizza in pizzas:
-    print(f"This is a {pizza.title()} pizza")
-print("Those are all the pizzas\n")
-
-# 4.2. Animals
-animals = ["dog","cat","rat"]
-for animal in animals:
-    print(f"A {animal} would make a great pet")
-print("These animals all have tails!")
+for value in range(6):
+    print(value)
