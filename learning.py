6425a959b0a6dafc26a3dd0a433c680203eee90e
Alexus Huang <alexushu2325@gmail.com>
2024-03-17 18:32:59 -0700

    Completed 4-6. Odd Numbers

--- learning.py
+++ learning.py
@@ -1,12 +1,19 @@
 #4-3. Counting to Twenty:
 for number in range(1,21):
     print(number)
+print("End of 4-3")
 
 #4-4. One Million
-numbers=[each_number for each_number in range(1,1000001)]
-print(numbers)
+# numbers=[each_number for each_number in range(1,1000001)]
+# print(numbers)
 
-#4-5. Summing a Million
-print(min(numbers))
-print(max(numbers))
-print(sum(numbers))
+# #4-5. Summing a Million
+# print(min(numbers))
+# print(max(numbers))
+# print(sum(numbers))
+
+#4-6 Odd Numbers
+odd_number_list=[]
+for odd_numbers in range(1,20,3):
+    odd_number_list.append(odd_numbers)
+print(odd_number_list)
