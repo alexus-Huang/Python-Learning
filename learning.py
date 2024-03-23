8d3ab27aa915f1936e4b7a3b252f4881696acda8
Alexus Huang <alexushu2325@gmail.com>
2024-03-22 21:45:45 -0700

    Learned about slicing and copying lists

--- learning.py
+++ learning.py
@@ -1,31 +1,5 @@
-#4-3. Counting to Twenty:
-for number in range(1,21):
-    print(number)
-print("End of 4-3")
-
-#4-4. One Million
-# numbers=[each_number for each_number in range(1,1000001)]
-# print(numbers)
-
-# #4-5. Summing a Million
-# print(min(numbers))
-# print(max(numbers))
-# print(sum(numbers))
-
-#4-6 Odd Numbers
-odd_number_list=[]
-for odd_numbers in range(1,20,3):
-    odd_number_list.append(odd_numbers)
-print(odd_number_list)
-
-
-#4-7. Threes
-threes=[]
-for odd in range(3,30,3):
-    threes.append(odd)
-print(threes)
-
-
-#4-8. Cubes
-cubes=[cube**3 for cube in range(1,10)]
-print(cubes)
+names = ["John", "Jack", "Bob","Pheobe","Lannan","Mike","Michael","Jansen","Kevin","Tristan","Chris","Kim"]
+copied_names =names[:]
+copied_names.append("Additional name")
+print(copied_names)
+print(names)
