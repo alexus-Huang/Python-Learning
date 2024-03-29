fa8ef258ab80006ad6f983c23dbe7d377bb9baf0
Alexus Huang <alexushu2325@gmail.com>
2024-03-28 22:24:49 -0700

    Completed 4-10. Slices from the Python Crash Course book

--- learning.py
+++ learning.py
@@ -1,5 +1,5 @@
-names = ["John", "Jack", "Bob","Pheobe","Lannan","Mike","Michael","Jansen","Kevin","Tristan","Chris","Kim"]
-copied_names =names[:]
-copied_names.append("Additional name")
-print(copied_names)
-print(names)
+# 4-10. Slices
+items=["pizza","hotdog","pretzel","pepporoni","sausage","bread","eggs","water"]
+print(f"The first 3 itmes in the list are: {items[:3]}")  # Print the first 3 items in a list
+print(f"Three items from the middle of the list are: {items[3:6]}") # Print the 3 items in the middle of the list
+print(f"The last 3 items in the list are: {items[-3:]}") # Print the last 3 items in the middle of the list
