b6e41b7ad1fe7e5ef97621c3108e7211fb0c7abf
Alexus Huang <alexushu2325@gmail.com>
2024-03-29 14:47:41 -0700

    Completed 4-13. Buffet practice from the Python Crash Course book

--- learning.py
+++ learning.py
@@ -1,5 +1,9 @@
-# Learning about tuples
-box_dimensions = (200,100)
-print(f"The width is {box_dimensions[0]} inches")
-print(f"The height is {box_dimensions[1]} inches")
-box_dimensions[0]=20
+# 4-13. Buffet
+simple_foods=("pizza","burger","ice cream","hot dog","salad")
+print("These are the basic foods served at this buffet:")
+for each_food in simple_foods:
+    print(each_food.title())
+menu_change=("new food item 1","new food item 2","burger","ice cream","hot dog","salad")
+print("\nThese are the new food items on the menu:")
+for new_food in menu_change:
+    print(new_food.title())
