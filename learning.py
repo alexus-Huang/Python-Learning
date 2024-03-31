e16cb3df5b2859fd119bcb3b291a7a8f35e96b55
Alexus Huang <alexushu2325@gmail.com>
2024-03-31 11:00:01 -0700

    Learned about testing multiple conditions

--- learning.py
+++ learning.py
@@ -1,11 +1,10 @@
-age = 100
-if age <5:
-    fair = 0
-elif age <18:
-    fair = 25
-elif age <50:
-    fair = 30
-else:
-    fair = 10
+toppings = ["veggies","meat","onions"]
 
-print(f"Your admission cost is ${fair}")
+if "veggies" in toppings:
+    print("Adding veggies")
+if "meat" in toppings:
+    print("Adding meat")
+if "cheese" in toppings:
+    print("Adding cheese")
+
+print("\n Done with the pizza")
