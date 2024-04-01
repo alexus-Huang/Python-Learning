92cd9f462bf06a963e816b338dccd8a886f90a30
Alexus Huang <alexushu2325@gmail.com>
2024-04-01 16:24:37 -0700

    Completed 5-5. Alien Colors #3 from Python Crash Course

--- learning.py
+++ learning.py
@@ -1,14 +1,24 @@
-#5-4. Alien Colors #2
-alien1_color = "green"
+#5-5. Alien Colors #3
+alien1_color, alien2_color, alien3_color = "green","yellow","red"
 if alien1_color == "green":
-    print("5 points for shooting a green alien!")
+    print("5 points")
+elif alien1_color == "yellow":
+    print("10 points")
 else:
-    print("10 points!")
+    print("15 points")
 
-print("\nSecond verson of the program:")
-alien2_color = "red"
+print("\nSecond Version:")
 if alien2_color == "green":
-    print("5 points for shooting a green alien!")
+    print("5 points")
+elif alien2_color == "yellow":
+    print("10 points")
 else:
-    print("10 points!")
+    print("15 points")
 
+print("\nThird Version:")
+if alien3_color == "green":
+    print("5 points")
+elif alien3_color == "yellow":
+    print("10 points")
+else:
+    print("15 points")
