2fa1019196c3dc78ab960d6663ad77a8d9f78b71
Alexus Huang <alexushu2325@gmail.com>
2024-04-01 16:26:52 -0700

    Completed 5-6. Stages of Life

--- learning.py
+++ learning.py
@@ -1,24 +1,15 @@
-#5-5. Alien Colors #3
-alien1_color, alien2_color, alien3_color = "green","yellow","red"
-if alien1_color == "green":
-    print("5 points")
-elif alien1_color == "yellow":
-    print("10 points")
-else:
-    print("15 points")
+#5-6. Stages of Life
+age = 2
 
-print("\nSecond Version:")
-if alien2_color == "green":
-    print("5 points")
-elif alien2_color == "yellow":
-    print("10 points")
-else:
-    print("15 points")
-
-print("\nThird Version:")
-if alien3_color == "green":
-    print("5 points")
-elif alien3_color == "yellow":
-    print("10 points")
-else:
-    print("15 points")
+if age <2:
+    print("You're a baby")
+elif age <4:
+    print("Toddler")
+elif age <13:
+    print("Kid")
+elif age <20:
+    print("Teenager")
+elif age <65:
+    print("Adult")
+elif age >= 65:
+    print("Elder")
