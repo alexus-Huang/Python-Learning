2949c29634e03efb0d5fc6fb3db6af99b0d6089b
Alexus Huang <alexushu2325@gmail.com>
2024-04-01 17:23:29 -0700

    Completed 5-8. Hello Admin

--- learning.py
+++ learning.py
@@ -1,9 +1,8 @@
-toppings = []
-if toppings:
-    for every_topping in toppings:
-        if every_topping =="cheese":
-            print("No cheese at the moment")
-        else:
-            print(f"Adding {every_topping.title()}")
-else:
-    print("Do you really want a plain pizza?")
+#5-8. Hello Admin
+usernames =["john","jake","admin","bob","steve"]
+for every_name in usernames:
+    if every_name == "admin":
+        print(f"Hello {every_name}")
+    else:
+        print(f"{every_name.title()} logged in")
+    
