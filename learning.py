2f075aad0d8bfaf332002928b04920ebd921d12c
Alexus Huang <alexushu2325@gmail.com>
2024-04-01 17:24:59 -0700

    Completed 5-9. No Users

--- learning.py
+++ learning.py
@@ -1,8 +1,17 @@
-#5-8. Hello Admin
-usernames =["john","jake","admin","bob","steve"]
-for every_name in usernames:
-    if every_name == "admin":
-        print(f"Hello {every_name}")
-    else:
-        print(f"{every_name.title()} logged in")
+# #5-8. Hello Admin
+usernames =[]
+# for every_name in usernames:
+#     if every_name == "admin":
+#         print(f"Hello {every_name}")
+#     else:
+#         print(f"{every_name.title()} logged in")
     
+#5-9. No Users
+if usernames:
+    for every_name in usernames:
+        if every_name == "admin":
+            print(f"Hello {every_name}")
+        else:
+            print(f"{every_name.title()} logged in")
+else:
+    print("We need some users!")
