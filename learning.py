11abc069f156e736241bbc4ab767cf3ac9f571d0
Alexus Huang <alexushu2325@gmail.com>
2024-04-02 21:53:06 -0700

    Solved a problem of the comparison not being case insensitive

--- learning.py
+++ learning.py
@@ -7,11 +7,22 @@ usernames =[]
 #         print(f"{every_name.title()} logged in")
     
 #5-9. No Users
-if usernames:
-    for every_name in usernames:
-        if every_name == "admin":
-            print(f"Hello {every_name}")
-        else:
-            print(f"{every_name.title()} logged in")
-else:
-    print("We need some users!")
+# if usernames:
+#     for every_name in usernames:
+#         if every_name == "admin":
+#             print(f"Hello {every_name}")
+#         else:
+#             print(f"{every_name.title()} logged in")
+# else:
+#     print("We need some users!")
+
+#5-10. Checking Usernames
+current_users = ["john","jake","BOB","steve","tristan","j"]
+new_users = ["Steve","bob","ashly","leila","moran","j"]
+lowerCased=[word.lower() for word in current_users]
+print(lowerCased)
+for every_new_user in new_users:
+    if every_new_user.lower() in lowerCased:
+        print(f"Matched name: {every_new_user.lower()}")
+    else:
+        print(f"This name isn't repeated: {every_new_user.lower()}")
