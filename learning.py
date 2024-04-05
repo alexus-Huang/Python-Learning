db791a73359e626991e260a3a29d58f8b4c416ae
Alexus Huang <alexushu2325@gmail.com>
2024-04-05 15:16:46 -0700

    Practiced with lists and looping through dictionaries

--- learning.py
+++ learning.py
@@ -1,28 +1,19 @@
-# # 6-1. Person
-# person1 = {
-#     "first_name": "john",
-#     "last_name" : "doe",
-#     "city" : "SF"
-# }
-# person1_firstName = person1["first_name"].title()
-# person1_lastName = person1["last_name"].title()
-# person1_city = person1["city"]
-# print(f"{person1_firstName} {person1_lastName} is from {person1_city}")
-
-# # 6.2 - Favorite Numbers
-# fav_nums = {
-#     "john": 1,
-#     "tristan": 2,
-#     "jack" : 3,
-#     }
-# print(f"John's favorite number is {fav_nums['john']}")
-# print(f"Tristan's favorite number is {fav_nums['tristan']}")
-# print(f"Jack's favorite number is {fav_nums['jack']}")
-
-#6-3. Glossary
-glossary = {
-    "print":"outputs values",
-    "title()": "capitalizes the first letter of a word"
+drivers_cars = {
+    "john":"BMW",
+    "jack":"ford",
+    "bob":"BMW",
+    "tristan":"ford"
 }
-print(f"Print is a function that {glossary['print']}")
-print(f"\nThe title function {glossary['title()']}")
+
+bmw_count = 0
+ford_count = 0
+for driver, car in drivers_cars.items():
+    if car == "ford":
+        ford_count +=1
+        print(f"{driver.title()} drives a {car.title()}")
+    else:
+        bmw_count +=1
+        print(f"{driver.title()} drives a {car.title()}")
+
+print(f"\nBMW count: {bmw_count}")
+print(f"Ford count: {ford_count}")
