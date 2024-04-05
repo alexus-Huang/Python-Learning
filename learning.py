4f4c0c8c89062e641d1a99320194cf0d24faaa7b
Alexus Huang <alexushu2325@gmail.com>
2024-04-04 17:10:48 -0700

    Completed 6-2. Favorite number

--- learning.py
+++ learning.py
@@ -1,3 +1,4 @@
+# 6-1. Person
 person1 = {
     "first_name": "john",
     "last_name" : "doe",
@@ -7,3 +8,13 @@ person1_firstName = person1["first_name"].title()
 person1_lastName = person1["last_name"].title()
 person1_city = person1["city"]
 print(f"{person1_firstName} {person1_lastName} is from {person1_city}")
+
+# 6.2 - Favorite Numbers
+fav_nums = {
+    "john": 1,
+    "tristan": 2,
+    "jack" : 3,
+    }
+print(f"John's favorite number is {fav_nums['john']}")
+print(f"Tristan's favorite number is {fav_nums['tristan']}")
+print(f"Jack's favorite number is {fav_nums['jack']}")
