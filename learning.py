8cde12a64df35846a16fc4773341e19af74b93a1
Alexus Huang <alexushu2325@gmail.com>
2024-04-04 17:13:21 -0700

    Completed 6-3. Favorite number

--- learning.py
+++ learning.py
@@ -1,20 +1,28 @@
-# 6-1. Person
-person1 = {
-    "first_name": "john",
-    "last_name" : "doe",
-    "city" : "SF"
-}
-person1_firstName = person1["first_name"].title()
-person1_lastName = person1["last_name"].title()
-person1_city = person1["city"]
-print(f"{person1_firstName} {person1_lastName} is from {person1_city}")
+# # 6-1. Person
+# person1 = {
+#     "first_name": "john",
+#     "last_name" : "doe",
+#     "city" : "SF"
+# }
+# person1_firstName = person1["first_name"].title()
+# person1_lastName = person1["last_name"].title()
+# person1_city = person1["city"]
+# print(f"{person1_firstName} {person1_lastName} is from {person1_city}")
 
-# 6.2 - Favorite Numbers
-fav_nums = {
-    "john": 1,
-    "tristan": 2,
-    "jack" : 3,
-    }
-print(f"John's favorite number is {fav_nums['john']}")
-print(f"Tristan's favorite number is {fav_nums['tristan']}")
-print(f"Jack's favorite number is {fav_nums['jack']}")
+# # 6.2 - Favorite Numbers
+# fav_nums = {
+#     "john": 1,
+#     "tristan": 2,
+#     "jack" : 3,
+#     }
+# print(f"John's favorite number is {fav_nums['john']}")
+# print(f"Tristan's favorite number is {fav_nums['tristan']}")
+# print(f"Jack's favorite number is {fav_nums['jack']}")
+
+#6-3. Glossary
+glossary = {
+    "print":"outputs values",
+    "title()": "capitalizes the first letter of a word"
+}
+print(f"Print is a function that {glossary['print']}")
+print(f"\nThe title function {glossary['title()']}")
