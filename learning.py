f354f09d8d5ea80c826d0d979be6751f3f0ef2cd
Alexus Huang <alexushu2325@gmail.com>
2024-04-04 17:00:03 -0700

    Completed 6-1. Person from Python Crash Course bok

--- learning.py
+++ learning.py
@@ -1,4 +1,9 @@
-car={"x_position":0,"y_pos":25,"brand":"BMW","speed":"slows"}
-print(car)
-del car["brand"]
-print(car)
+person1 = {
+    "first_name": "john",
+    "last_name" : "doe",
+    "city" : "SF"
+}
+person1_firstName = person1["first_name"].title()
+person1_lastName = person1["last_name"].title()
+person1_city = person1["city"]
+print(f"{person1_firstName} {person1_lastName} is from {person1_city}")
