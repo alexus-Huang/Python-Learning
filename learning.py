5c0822855e3719df143c2fa1e97e9446cca07b7c
Alexus Huang <alexushu2325@gmail.com>
2024-04-05 15:38:59 -0700

    Used the keys() method

--- learning.py
+++ learning.py
@@ -5,15 +5,5 @@ drivers_cars = {
     "tristan":"ford"
 }
 
-bmw_count = 0
-ford_count = 0
-for driver, car in drivers_cars.items():
-    if car == "ford":
-        ford_count +=1
-        print(f"{driver.title()} drives a {car.title()}")
-    else:
-        bmw_count +=1
-        print(f"{driver.title()} drives a {car.title()}")
-
-print(f"\nBMW count: {bmw_count}")
-print(f"Ford count: {ford_count}")
+for name in drivers_cars.keys():
+    print(f"Names of everyone in the dictionary: {name.title()}")
