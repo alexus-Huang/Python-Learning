9b5450f55519bf92b24e5218d13f48af415c0b58
Alexus Huang <alexushu2325@gmail.com>
2024-04-14 18:19:53 -0700

    Completed 6-7.People from Python Crash Course bok

--- learning.py
+++ learning.py
@@ -1,7 +1,24 @@
-alien1 = {"color":"green","points":5}
-alien2 = {"color":"blue","points":10}
-alien3= {"color":"red","points":15}
+#6-7 People
+people = {
+    "john":{
+        "first":"john",
+        "last" : "doe",
+        "age": 30
+    },
+    "jack":{
+        "first":"jack",
+        "last":"brown",
+        "age":35
+    },
+    "jane":{
+        "first":"jane",
+        "last":"doe",
+        "age":40
+    }
+}
 
-aliens=[alien1,alien2,alien3]
-for alien in aliens:
-    print(alien)
+for every_person,detail in people.items():
+    print(f"Name: {every_person}")
+    full_name = f"{detail['first']} {detail['last']}"
+    age = f"{detail['age']}"
+    print(f"Description:\n\t{full_name.title()}\n\tAge: {age}")
