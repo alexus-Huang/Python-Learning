2fc34c7a6a342381c98bc3160f1a750a14c09cb1
Alexus Huang <alexushu2325@gmail.com>
2024-04-21 21:37:02 -0700

    Completed 7-3. Multiples of ten

--- learning.py
+++ learning.py
@@ -2,9 +2,18 @@
 # car_type = input("What kind of rental car do you want: ")
 # print(f"Let me see if I can find a {car_type}")
 
-#7-2. Restaurant Seating
-dinner_group = int(input("How many people are dining? "))
-if dinner_group > 8:
-    print("You'll have to wait for a table")
+# #7-2. Restaurant Seating
+# dinner_group = int(input("How many people are dining? "))
+# if dinner_group > 8:
+#     print("You'll have to wait for a table")
+# else:
+#     print(f"Your table is ready for {dinner_group} people")
+
+#7-3. Multiples of ten
+number = int(input("Enter in a number to see if its a multiple of 10 or not: "))
+if number % 10 == 0:
+    print(f"{number} is a multiple of 10")
 else:
-    print(f"Your table is ready for {dinner_group} people")
+    print(f"{number} is not a multiple of 10")
+
+
