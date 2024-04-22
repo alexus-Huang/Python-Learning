3703115dd317bd3cd6e03f29d2ea0991a263b1c2
Alexus Huang <alexushu2325@gmail.com>
2024-04-21 20:04:40 -0700

    Learned about the input() function

--- learning.py
+++ learning.py
@@ -1,4 +1,2 @@
-#6-10. Favorite Numbers
-fav_numbers = {"john":5,"jack":4,"bob":4}
-john_favNum = fav_numbers.get("john","John wasn't assigned a value")
-print(john_favNum)
+message = input("Type in what you want to repeat:")
+print(message)
