99197f00924cec35d5e38a86b8fa8af6e8b33bfc
Alexus Huang <alexushu2325@gmail.com>
2024-04-21 20:42:18 -0700

    Completed 7-1. Rental Car from Python Crash Course book

--- learning.py
+++ learning.py
@@ -1,2 +1,3 @@
-message = input("Type in what you want to repeat:")
-print(message)
+#7-1. Rental Car.
+car_type = input("What kind of rental car do you want: ")
+print(f"Let me see if I can find a {car_type}")
