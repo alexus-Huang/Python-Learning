8e93d1a3db5234a83bd74be429c31266c7cd674f
Alexus Huang <alexushu2325@gmail.com>
2024-04-22 23:05:03 -0700

    Learned about while loops

--- learning.py
+++ learning.py
@@ -1,19 +1,12 @@
-# #7-1. Rental Car.
-# car_type = input("What kind of rental car do you want: ")
-# print(f"Let me see if I can find a {car_type}")
+# While loops
+prompt = "Type in a word that you want to repeat\nEnter 'quit' to end the program: "
+message = ""
 
-# #7-2. Restaurant Seating
-# dinner_group = int(input("How many people are dining? "))
-# if dinner_group > 8:
-#     print("You'll have to wait for a table")
-# else:
-#     print(f"Your table is ready for {dinner_group} people")
-
-#7-3. Multiples of ten
-number = int(input("Enter in a number to see if its a multiple of 10 or not: "))
-if number % 10 == 0:
-    print(f"{number} is a multiple of 10")
-else:
-    print(f"{number} is not a multiple of 10")
+while message != "quit":
+    message = input(prompt)
+    if message != "quit":
+        print(message)
+    else:
+        print(f"Program is quitting because you entered {message}")
 
 
