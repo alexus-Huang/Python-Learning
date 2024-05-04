30474e46a603e991676bd98e087cd74830f35a0b
Alexus Huang <alexushu2325@gmail.com>
2024-05-04 10:15:44 -0700

    Learaned about using 'continue'

--- learning.py
+++ learning.py
@@ -1,9 +1,8 @@
-prompt = "Type out the letter f to continue: "
-active = True
-
-while active:
-    message = input(prompt)
-    if message != "f":
-        active = False
-    else:
+odd_numbers = []
+current_numbers = 0
+while current_numbers < 10:
+    current_numbers+=1
+    if current_numbers % 2 == 0:
         continue
+    odd_numbers.append(current_numbers)
+print(odd_numbers)
