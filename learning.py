c1e8e18ae27e961493c7b732d38a7e039e673b17
Alexus Huang <alexushu2325@gmail.com>
2024-06-03 21:04:58 -0700

    Finished 8-4. Large shirts

--- learning.py
+++ learning.py
@@ -1,9 +1,7 @@
-def make_shirt(size,message):
+def make_shirt(size="large",message="Python"):
     print(f"\nYour shirt size is {size}")
     print(f"The messaage being printed is {message}")
 
-#Positonal Arguments
-make_shirt("Medium","T-shirt")
-
-#Keyword Arguments
-make_shirt(size="Small",message="Small T-shirt")
+make_shirt()
+make_shirt("medium")
+make_shirt("small","JS")
