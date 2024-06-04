dcecb582e79e6a2e782fa9fa811a7ba2b1aff74f
Alexus Huang <alexushu2325@gmail.com>
2024-06-04 16:52:36 -0700

    Completed 8-12. Sandwiches

--- learning.py
+++ learning.py
@@ -1,22 +1,7 @@
-# #8-9. Messages
-# short_text_msgs = ["hello","hi","a messaage"]
-# printed_text_msgs = []
-# def show_messages(text_msgs):
-#     while text_msgs:
-#         show_text_msgs = text_msgs.pop()
-#         print(f"{show_text_msgs}")
-#         printed_text_msgs.append(show_text_msgs)
+#8-12. Sandwiches
+def build_sandwich(*toppings):
+    print(f"Here is what you want on your toppings")
+    for each_topping in toppings:
+        print(f"- {each_topping}")
 
-# show_messages(short_text_msgs)
-
-#8-10. Sending Messages:
-send_text_msgs = ["hello","hi","a messaage"]
-sent_txt_msgs = []
-def show_messages(text_msgs):
-    while text_msgs:
-        show_text_msgs = text_msgs.pop()
-        print(f"{show_text_msgs}")
-        sent_txt_msgs.append(show_text_msgs)
-show_messages(send_text_msgs)
-print(send_text_msgs)
-print(sent_txt_msgs)
+build_sandwich("cheese","lettuce","tomatoes")
