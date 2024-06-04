f0148825a1dac69fcfd63e74bc344283b4855e61
Alexus Huang <alexushu2325@gmail.com>
2024-06-04 10:12:19 -0700

    8-8. User Albums

--- learning.py
+++ learning.py
@@ -4,8 +4,16 @@ def make_album(artist_name, album_title,songs=None):
         album["songs"] = songs
     return album
 
-first_album = make_album("John","John's Album Title")
-print(first_album)
-
-second_album = make_album("Dave","Dave's Album Title",90)
-print(second_album)
+while True:
+    print(f"\nTo quit at anytime, enter 'q'")
+    album_artist_name = input("What is the artist's name?")
+    if album_artist_name == "q":
+        break
+    album_title = input("What is the album's title?")
+    if album_title == "q":
+        break
+    album_songs = input("How many songs are there in this album?")
+    if album_songs == "q":
+        break
+    new_album= make_album(album_artist_name,album_title,album_songs)
+    print(new_album)
