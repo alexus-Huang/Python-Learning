dcb03d36c77dc7f2f1c71df080e6ae5a768ced33
Alexus Huang <alexushu2325@gmail.com>
2024-06-06 21:40:31 -0700

    Completed 9-6. Ice Cream Stand

--- learning.py
+++ learning.py
@@ -1,29 +1,12 @@
-class Car:
-    def __init__(self,make,model,year):
-        self.make = make
-        self.model = model
-        self.year = year
-
-    def describe_car(self):
-        description = f"{self.make} {self.model} {self.year}"
-        return description
-
-    def fill_gas_tank(self):
-        print("Filling up gas!")
-
-class Battery:
-    def __init__(self,battery_cap,battery_size=59):
-        self.battery_size = battery_size
-        self.battery_cap = battery_cap
+#9-6. Ice Cream Stand
+class IceCreamStand:
+    def __init__(self,*flavors) -> None:
+        self.flavors = flavors
     
-    def battery_level(self):
-        print(f"Battery: {self.battery_size}")
+    def menu(self):
+        print(f"Here are each flavors:")
+        for each_flavor in self.flavors:
+            print(f"{each_flavor.title()}")
 
-class ElectricCar(Car):
-    def __init__(self, make, model, year):
-        super().__init__(make, model, year)
-        self.battery = Battery(2)
-
-
-
-
+my_stand = IceCreamStand("vanilla","chocolate")
+my_stand.menu()
