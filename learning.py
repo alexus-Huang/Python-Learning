bb1a105496bc284d4c1f3d0e3422571df28e0c45
Alexus Huang <alexushu2325@gmail.com>
2024-06-06 19:40:58 -0700

    Learaned about inheritance aand composition

--- learning.py
+++ learning.py
@@ -1,26 +1,29 @@
-class Login:
-    def __init__(self,login_attempts):
-        self.login_attempts = login_attempts
+class Car:
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def describe_car(self):
+        description = f"{self.make} {self.model} {self.year}"
+        return description
+
+    def fill_gas_tank(self):
+        print("Filling up gas!")
+
+class Battery:
+    def __init__(self,battery_cap,battery_size=59):
+        self.battery_size = battery_size
+        self.battery_cap = battery_cap
     
-    def login_attempt(self,increase):
-        self.login_attempts += increase
-    
-    def reset_login_attempts(self):
-        self.login_attempts = 0
+    def battery_level(self):
+        print(f"Battery: {self.battery_size}")
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery(2)
 
 
-new_login = Login(0)
 
-print(new_login.login_attempts)
 
-new_login.login_attempt(1)
-
-print(new_login.login_attempts)
-
-new_login.login_attempt(1)
-
-print(new_login.login_attempts)
-
-new_login.reset_login_attempts()
-
-print(new_login.login_attempts)
