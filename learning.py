1e0523861d5e2e209286f89dedf3b9a602110098
Alexus Huang <alexushu2325@gmail.com>
2024-06-07 18:59:14 -0700

    Completed 9-15. Lottery Analysis

--- learning.py
+++ learning.py
@@ -1,25 +1,29 @@
-class User:
-    def __init__(self,first_name,last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+from random import choice
+lottery = [1,2,3,4,5,6,7,8,9,10,"a","b","c","d","e"]
+chosen_lottery = []
+my_ticket = ["a",1]
+counter = 0
+class Lottery:
+    """Picks out random letters and numbers from a list and tries to match it with a set list. Counts how many tries it took."""
+    def __init__(self,lottery_selection = lottery,selections = 2):
+        self.lottery_selection = lottery_selection
+        self.selections = selections
+        self.status = True
     
-    def full_name(self):
-        print(f"{self.first_name} {self.last_name}")
-    
+    def selection(self,counter = counter):
+        while self.status:
+            for selections_chosen in range (self.selections): # repeat however many times depending on the amount of selections
+                chosen_shit = choice(lottery)  # randoly picks out stuff from the list
+                chosen_lottery.append(chosen_shit) # adds the chosen lottery stuff to the list
+            print(chosen_lottery)
+            if chosen_lottery == my_ticket:
+                print(f"You won! Chosen lottery: {chosen_lottery} - Your Ticket: {my_ticket}")
+                print(f"It took {counter} tries.")
+                self.status = False
+            else:
+                chosen_lottery.clear()
+                counter += 1
+                continue
 
-class Privileges:
-    def __init__(self,*commands):
-        self.admin_commands = commands
-    
-    def show_commands(self):
-        print(f"Commands: {self.admin_commands}")
-
-
-class Admin(User):
-    def __init__(self, first_name, last_name):
-        super().__init__(first_name, last_name)
-        self.commands = Privileges("delete","post","edit")
-
-new_admin = Admin("john","doe")
-new_admin.commands.show_commands()
-
+lottery_one = Lottery()
+lottery_one.selection()
